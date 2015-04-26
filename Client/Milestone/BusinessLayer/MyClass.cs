6f890579e1d877b304a9d677ef470840036f4734
smilevn99 <smilevn99@gmail.com>
2015-04-26 15:33:52 -0700

    Client
    Init client project

---
+++ Client/Milestone/BusinessLayer/MyClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.IO;
+
+namespace BusinessLayer
+{
+	public class MyClass
+	{
+		public MyClass ()
+		{
+		}
+
+		public string getWord()
+		{
+			return "Hello World";
+		}
+
+		public async void FetchDataAsync()
+		{
+			var request = (HttpWebRequest) WebRequest.Create(new Uri("abc"));
+			request.Method = "GET";
+
+			using (WebResponse response = await request.GetResponseAsync ()) 
+			{
+				// TODO
+			}
+		}
+	}
+}
+
